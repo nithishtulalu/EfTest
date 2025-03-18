3b2b67bd79697c442b7d42241a1f1337e68fcdfc
nithish <nithishtulalu121@gmail.com>
2025-03-19 00:14:23 +0530

    migratin by nithish

---
+++ Migrations/20250318184343_nithish.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFTEST.Migrations
+{
+    /// <inheritdoc />
+    public partial class nithish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
